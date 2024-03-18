96b51b324db201a057f0dab2f878e6f71e93ab82
Ahmed Hazzaa <ahazzaa@arabdt.com>
2024-03-19 00:57:43 +0200

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by A.Hazzaa on 19/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
